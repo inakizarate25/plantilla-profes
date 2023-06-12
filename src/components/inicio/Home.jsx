5fa5844bd31e42c7cc6e796d745256f8de7f3f75
inakizarate25 <inakizarate25@gmail.com>
2023-06-12 09:55:38 -0300

    arreglos

--- src/components/inicio/Home.jsx
+++ src/components/inicio/Home.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <section className="h-auto w-full flex flex-col items-start px-3 text-gray-50 py-10 my-10 gap-4 md:h-[90vh] md:justify-center md:px-[300px]  md:w-full md:my-2 " id='inicio'>
+    <section className="h-auto w-full flex flex-col items-start px-5 text-gray-50 py-10 my-10 gap-4 md:h-[60vh] md:justify-center md:items-center   max-w-4xl" id='inicio'>
         <div className='bg-neutral-700 w-auto h-auto rounded-3xl py-2 px-3'><span className='bg-orange-500 px-2  py-1 rounded-xl font-semibold'>15% OFF</span> El plan mas eficiente para tu meta</div>
-        <h1 className='text-[10rem] w-[500px] font-semibold text-left'>Personal Training</h1>
-        <p className='py-5 text-xl text-neutral-300 font-medium md:text-center max-w-3xl'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        <div className='flex justify-center md:justify-start items-center w-full gap-6'>
-            <a href="#comenzar" className='px-5 py-4 bg-orange-500 rounded-lg font-bold text-2xl hover:scale-90 transition-all'>Comenzar</a>
+        <h1 className='text-7xl font-semibold'>Personal Training</h1>
+        <p className='py-5 text-xl text-neutral-300 font-medium md:text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <div className='flex justify-center items-center w-full gap-6'>
+            <a href="#planes" className='px-5 py-4 bg-orange-500 rounded-lg font-bold text-2xl hover:scale-90 transition-all'>Comenzar</a>
             <a href="#testimonios" className='px-5 py-4 bg-neutral-700 rounded-lg font-bold text-2xl hover:scale-90 transition-all'>Testimonios</a>
         </div>
     </section>
--- src/components/planes/Planes.jsx
+++ src/components/planes/Planes.jsx
@@ -3,12 +3,11 @@ import React from 'react'
 const Planes = ({img, titulo, descripcion, precio}) => {
   return (
     <article className='w-full h-auto py-4 px-5 flex flex-col items-start shadow-lg rounded-md gap-2 md:w-96'>
-       
-      <img src={img} alt="training" className='w-full h-auto md:w-96 rounded-lg'/>
-      <h3 className='text-3xl font-semibold'>{titulo}</h3>
+       <h3 className='text-3xl font-semibold'>{titulo}</h3>
+      <img src={img} alt="training" className='w-full h-auto  rounded-lg'/>
       <div className='flex justify-center items-center'>{descripcion}</div>
       <p className='text-2xl font-semibold '>{precio}</p>
-      <button className='px-4 py-3 text-2xl font-semibold bg-orange-600 rounded-lg hover:scale-90 transition-all'>Adquirir</button>
+      <button className='px-4 py-3 text-2xl font-semibold bg-orange-600 rounded-lg hover:scale-90 transition-all self-center'>Adquirir</button>
     </article>
   )
 }
