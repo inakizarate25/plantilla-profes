3604f28f534b6f2f5d04c487ee6f59adcbd47fbf
inakizarate25 <inakizarate25@gmail.com>
2023-06-12 11:29:51 -0300

    rrr

--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -2,8 +2,8 @@
 
 const Blogs = ({img, titulo, descripcion}) => {
   return (
-   <article className="w-full h-auto py-4 px-5 flex flex-col items-start shadow-lg rounded-md gap-2 animate-fade-right animate-once animate-ease-in-out">
-    <img src={img} alt="training" className="w-full h-auto md:w-72 rounded-lg" />
+   <article className="w-full md:w-[400px] h-auto py-4 px-5 flex flex-col items-start shadow-lg rounded-md gap-2 animate-fade-right animate-once animate-ease-in-out">
+    <img src={img} alt="training" className="w-full h-auto  rounded-lg" />
     <h3  className='text-3xl font-semibold'>{titulo}</h3>
     <p className="text-lg font-semibold text-neutral-500">{descripcion}</p>
     <a href="/" className="px-5 py-3 text-2xl font-semibold bg-orange-600 rounded-lg hover:scale-90 transition-all">Leer</a>
